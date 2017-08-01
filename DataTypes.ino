37165ee6b5493c53476c9501f3faa4c171361d40
Sandipon Paul <psandipon@gmail.com>
2017-08-01 23:30:51 +0600

    Create DataTypes.ino

---
+++ DataTypes.ino
@@ -0,0 +1,18 @@
+boolean val = false ;
+boolean state = true ;
+
+Char chr_a = ‘a’ ;
+Char chr_c = 97 ;
+
+byte m = 25 ;
+
+int counter = 32 ;
+Unsigned int counter = 60 ;
+
+word w = 1000 ;
+
+Long velocity = 102346 ;
+Unsigned Long velocity = 101006 ;
+
+float num = 1.352;
+double num = 45.352 ;
